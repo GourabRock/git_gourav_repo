37464a411564c98670b5989cb8a64bf739f43966
GourabRock <166551882+GourabRock@users.noreply.github.com>
2024-07-02 13:21:27 +0530

    text deleted

--- methodsdemo2.py
+++ methodsdemo2.py
@@ -71,22 +71,3 @@ Learn from Experienced Industry Experts!
 DevOps Engineer Masters ProgramEXPLORE PROGRAMLearn from Experienced Industry Experts!
 Let us now look into the Git commands that may play a significant role in resolving conflicts.
 
-Git Commands to Resolve Conflicts
-1. git log --merge 
-The git log --merge command helps to produce the list of commits that are causing the conflict.
-
-2. git diff 
-The git diff command helps to identify the differences between the states repositories or files.
-
-3. git checkout 
-The git checkout command is used to undo the changes made to the file, or for changing branches.
-
-4. git reset --mixed 
-The git reset --mixed command is used to undo changes to the working directory and staging area.
-
-5. git merge --abort
-The git merge --abort command helps in exiting the merge process and returning back to the state before the merging began.
-
-6. git reset
-The git reset command is used at the time of merge conflict to reset the conflicted files to their original state.
-
